208c902c44236f26232bf30582b58ace5564a56c
alejandro <aleblancogovantes@hotmail.com>
2022-12-23 09:39:55 +0100

    added text on about us and styles

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,20 +1,47 @@
-
-
-
 const AboutUsPage = () => {
-    return(
-        <div>
-            <h1>About us page</h1>
-     
-        <p>Lorem ipsum dolor s
-           pulvinar urna eget est fringilla tincidunt. Aliquam tempor nec quam vitae
-            condimentum. Nullam congue gravida ex, ac facilisis urna volutpat ut. Nam purus nulla, <br />
-             pharetra eu euismod a, dignissim eget risus. In eu velit lacus. Donec vel ultrices neque, 
-             eu efficitur libero. Nullam quis purus finibus, imperdiet tortor a, vehicula dui. Mauris 
-            rsus. Fusce sit amet riero ex. </p>
+  return (
+    <div>
+      <h1>About us page</h1>
 
-        </div>
-    )
-}
+      <p>
+        Lorem ipsum dolor s pulvinar urna eget est fringilla tincidunt. Aliquam
+        tempor nec quam vitae condimentum. Nullam congue gravida ex, ac
+        facilisis urna volutpat ut. Nam purus nulla, <br />
+        pharetra eu euismod a, dignissim eget risus. In eu velit lacus. Donec
+        vel ultrices neque, eu efficitur libero. Nullam quis purus finibus,
+        imperdiet tortor a, vehicula dui. Mauris rsus. Fusce sit amet riero ex.
+      </p>
 
-export default AboutUsPage; 
+      <h2> hffjh jgkf igjfk</h2>
+      <p>
+        ondimentum. Nullam congue gravida ex, ac facilisis urna volutpat ut. Nam
+        purus nulla, <br />
+        pharetra eu euismod a, dignissim eget risus. In eu velit lacus. Donec
+        vel ultrices neque, eu efficitur libero. Nullam quis purus finibus,
+        imperdiet tortor a, vehicula dui. Mauris rsus. Fusce sit amet riero ex.
+      </p>
+      <ul className="about_us_list">
+        <li>
+          <h3> neque jgkf nbdjufn</h3>
+          <p>
+            ondimentum. Nullam congue gravida ex, ac facilisis urna volutpat ut.
+            Nam purus nulla, <br />
+            pharetra eu euismod a, dignissim eget risus. In eu velit lacus.
+            Donec vel ultrices neque,
+          </p>
+        </li>
+        <li>
+          <h3>ondimentum</h3>
+          <p>
+            ondimentum. Nullam congue gravida ex, ac facilisis urna volutpat ut.
+            Nam purus nulla, <br />
+            pharetra eu euismod a, dignissim eget risus. In eu velit lacus.
+            Donec vel ultrices neque,
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUsPage;
