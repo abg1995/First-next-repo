49464ad9f65d2b0dcbe674c539b46c42469cdd02
alejandro <aleblancogovantes@hotmail.com>
2023-01-02 12:51:00 +0100

    added styles for front page, modified h2 titles, removed h1 title, added div with card className, added styles to h2 tag and changed positioning

--- src/components/events/events-page.jsx
+++ src/components/events/events-page.jsx
@@ -1,27 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-import react from "react";
 
 
-function AllEvents({data}){
+function AllEvents({ data }) {
+  return (
+    <>
+      <div className="events_page">
+         <br />
+        <br />
+          {data.map((ev) => (
+            <Link key={ev.id} href={`/events/${ev.id}`} passHref>
+                <div className="card">
+              <Image src={ev.image} width={400} height={400} />
+              <h2>{ev.title}</h2>
+              </div>
+            </Link>
+          ))}
 
-    return(
-        <>
-         <div className="events_page">
-         <h1>Events index</h1>
-
-         <a href="/">Get home</a> <br /><br /><br />
-        <div>
-            {data.map( ev => (
-                <Link key={ev.id} href={`/events/${ev.id}`}>
-                <Image src={ev.image} width={300} height={300}  /> <h2>{ev.title}</h2></Link>
-            ))}
-    
-
-        </div>
-        </div> 
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default AllEvents;
