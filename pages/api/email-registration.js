8a81f5902be67fed7b5261b038e33842166e33e1
alejandro <aleblancogovantes@hotmail.com>
2023-01-04 12:05:53 +0100

    addes sass styles, conected back tto front, created api file email-registration, added backend , added logic in single event jsx

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,5 +1,65 @@
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+  /* path is imported as a module 
+    cwd = current working directory to access the directory we are working on, 
+    so we tell the PATH TO FOLLOW, in this case process on cwd (EVENTS_APP), to go folder data,
+    and access data.json file 
+    fs module allows to read and overwrite data in files*/
+  return path.join(process.cwd(), "data", "data.json");
+}
+
+//first we need to extract the data so we can find the rest and overwrite it
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData); // <-- this is to convert data into js object
+  return data;
+}
+
+export default function handler(req, res, next) {
+  const { method } = req;
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath); //this is how we extract data
+
+  //this is how we send 404 message in case of no data
+  if (!allEvents) {
+    return res.status(404).json({
+      status: 404,
+      message: " Events data not found",
+    });
+  }
+
+  if (method === "POST") {
+    const { email, eventId } = req.body;
+
+  if( !email || !email.include('@')){
+    res.status(422).json({message: 'invalid email address'});
+    return;
+  }
 
 
-export default function handler(req,res,next) {
-    
+    const newAllEvents = allEvents.map( ev => {
+        if(ev.id === eventId){
+            if(ev.emails_registered.includes(email)){
+                res.status(409).json({message: 'this email is already registered'})
+                return ev;
+            }
+            return {
+                ...ev, emails_registered: [...ev.emails_registered, email],
+            };
+        }
+        return ev;
+    })
+
+    fs.writeFileSync(filePath, JSON.stringify({events_categories, allEvents: newAllEvents}))
+
+
+    res.status(201).json({
+      message: `you have been registed succesfuly with the email: ${email} 
+       for the event: ${eventId}`,
+    });
+  }
 }
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,7 +1,43 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import {useRef, useState} from "react";
 
 function SingleEvent({ data }) {
-    const onSubmit = () => {}
+    const inputEmail = useRef();
+    const router = useRouter();
+    const [message, setMessage] = useState('');
+
+    console.log(router);
+
+    const onSubmit = async(e) => {
+      e.preventDefault();
+      const emailValue = inputEmail.current.value;
+      const eventId = router?.query.id;
+
+      const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+      if(!emailValue.match(validRegex)){
+        setMessage('Please introduce a correct email address')
+      }
+
+      try{
+        const response = await fetch('/api/email-registration', {
+          method: 'POST',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify({email: emailValue, eventId}) 
+        });
+
+        if(!response.ok)throw new Error(`Error: ${response.status}`)
+          const data = await response.json();
+          setMessage(data.message); 
+          inputEmail.current.value = '';
+      }catch (e) {
+        console.log('ERROR',e)
+      }
+    }
+
   return (
     <>
       <div className="event_single_page">
@@ -10,13 +46,14 @@ function SingleEvent({ data }) {
         <p>{data.description}</p>
         <form onSubmit={onSubmit} className='email_registration'>
           <label>Get registerd for this event </label>
-          <input
+          <input ref={inputEmail}
             type="email"
             id="email"
             placeholder="Please insert your email here"
           />
-          <button type="button">Submit</button>
+          <button type="submit">Submit</button>
         </form>
+        <p>{message}</p>
       </div>
     </>
   );
